fd7867789911b1f27b899a3e2b9b51e4d61ba459
Danny <danielhjpark@gmail.com>
2024-11-09 03:21:27 +0900

    NEW::드래그드롭

---
+++ Assets/Scripts/UI/Slot.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IDropHandler
+{
+    GameObject Icon()
+    {
+        if(transform.childCount > 0)
+        {
+            return transform.GetChild(0).gameObject;
+        }
+        else
+        {
+            return null;
+        }
+    }
+    public void OnDrop(PointerEventData eventData)
+    {
+        if (Icon() == null)
+        {
+            DragAndDrop.beingDraggedIcon.transform.SetParent(transform);
+            DragAndDrop.beingDraggedIcon.transform.position = transform.position;
+        }
+    }
+}
