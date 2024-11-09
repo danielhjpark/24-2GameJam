713520d8dcc186c0fbf284412fd7927e198a7d22
Danny <danielhjpark@gmail.com>
2024-11-10 02:35:24 +0900

    FIX::2스테이지 대화 수정

--- Assets/Scripts/UI/Camera/CameraManager.cs
+++ Assets/Scripts/UI/Camera/CameraManager.cs
@@ -70,7 +70,7 @@ public class CameraManager : MonoBehaviour
     IEnumerator delay()
     {
         GameManager.Instance.Player.gameObject.SetActive(false);
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(1.5f);
         stageChat.ShowDialogue();
     }
     void LimitCameraArea()
