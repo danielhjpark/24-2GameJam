b70f91c07a0116f3eebc829702327c178f801b1b
오민근 <83389506+dhalsrms1230@users.noreply.github.com>
2024-11-10 13:39:54 +0900

    여러 개 수정

--- Assets/Scripts/Stage/Stage_3/Object/PoketWatch.cs
+++ Assets/Scripts/Stage/Stage_3/Object/PoketWatch.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PoketWatch : ItemBase
 {
     [SerializeField]
     private CameraManager _cameraManager;
+    [SerializeField]
+    private string NextSceneName = "Ending";
 
     public override void UseItem()
     {
         // 게임 클리어
         Destroy(this.gameObject);
+        SceneManager.LoadScene(NextSceneName);
+
         Debug.Log("게임 클리어");
     }
 }
